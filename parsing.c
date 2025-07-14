30fb28f745c39ab280a6aad8283e14ef95ca4754
viljam ahdekivi <vahdekiv@student.hive.fi>
2025-07-14 15:52:21 +0300

    quiet day

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: vahdekiv <vahdekiv@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 13:15:13 by vahdekiv          #+#    #+#             */
-/*   Updated: 2025/07/08 13:16:27 by vahdekiv         ###   ########.fr       */
+/*   Updated: 2025/07/14 15:29:05 by vahdekiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,29 @@ int	ft_parse_args()
 
 }
 
-int	ft_parse_cmds()
+char	*get_path(char *env, char *path)
 {
+	int	i;
 
+	i = 0;
+	while (env[i])
+	{
+		ft_substr(ft_strcmp(env[i], "PATH"), env[i], ft_strlen(env[i]));
+		i++;
+	}
+}
+
+int	ft_parse_cmds(char *cmd)
+{
+	char	*path;
+	char	**split;
+
+	path = get_path(env, "PATH");
+	split = ft_split(path, ":");
+	while (split[i])
+	{
+		if (ft_strcmp(split[i], cmd) == 0)
+			return (split[i]);
+		i++;
+	}
 }
